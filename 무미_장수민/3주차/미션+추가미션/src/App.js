42aa346e7fac2539ea0422dbe9fa1f2dc2023017
장수민 <162973960+suminn9@users.noreply.github.com>
2024-06-02 16:46:24 +0900

    Update App.js

--- 무미_장수민/3주차/미션+추가미션/src/App.js
+++ 무미_장수민/3주차/미션+추가미션/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
+import styled from 'styled-components'; // styled-components import 추가
 import Navbar from './Navbar';
 import Footer from './Footer';
 import MainPage from './MainPage';
@@ -12,9 +12,9 @@ import MovieDetailPage from './MovieDetailPage';
 import { fetchPopularMovies } from './MovieApi';
 
 const AppContainer = styled.div`
-  background-color: #1c1c1c;
+  background-color: #1c1c1c; // 어두운 배경 색상 설정
   color: white;
-  min-height: 100vh;
+  min-height: 100vh; // 화면 전체 높이 설정
 `;
 
 function App() {
@@ -34,19 +34,19 @@ function App() {
 
   return (
     <AppContainer>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MainPage movies={movies} />} />
-        <Route path="/popular" element={<PopularPage />} />
-        <Route path="/nowplaying" element={<NowPlayingPage />} />
-        <Route path="/toprated" element={<TopRatedPage />} />
-        <Route path="/upcoming" element={<UpComingPage />} />
-        <Route path="/movie/:id" element={<MovieDetailPage />} />
-        <Route path="/signup" element={<div>회원가입 페이지</div>} />
-      </Routes>
-      <Footer />
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage movies={movies} loading={loading} />} /> {/* loading 상태 전달 */}
+          <Route path="/popular" element={<PopularPage />} />
+          <Route path="/nowplaying" element={<NowPlayingPage />} />
+          <Route path="/toprated" element={<TopRatedPage />} />
+          <Route path="/upcoming" element={<UpComingPage />} />
+          <Route path="/movie/:id" element={<MovieDetailPage />} />
+          <Route path="/signup" element={<div>회원가입 페이지</div>} />
+        </Routes>
+        <Footer />
+      </Router>
     </AppContainer>
   );
 }
